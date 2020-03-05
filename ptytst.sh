a570c4688e3d04a82c23e3ae09ce1afaf2d74625
lord-nt <61818304+lord-nt@users.noreply.github.com>
2020-03-05 07:27:41 -0500

    Update ptytst.sh

--- ptytst.sh
+++ ptytst.sh
@@ -3,7 +3,7 @@
 #
 # !!Requires socat and netcat to function!!
 #
-# run ser2telsvr.sh once to see pty# generated then adjust the ksnetcat.sh and vmodem.sh scripts accordingly.
+# run ptytst.sh once to see pty# generated then adjust the ksnetcat.sh and vmodem.sh scripts accordingly.
 #
 # the "1st pty#" is for the "ptsnum=" variable in "ksnetcat.sh" the "2nd pty#" is for vmodem.sh. Example for vmodem.sh port: serport=pty/6
 socat -d  -d pty,raw,echo=0 pty,raw,echo=0 &
