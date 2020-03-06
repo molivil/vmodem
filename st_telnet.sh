64dc1eb22b0cde578434153cb46d5ebbae7a4148
lord-nt <61818304+lord-nt@users.noreply.github.com>
2020-03-06 13:39:30 -0500

    Add files via upload

---
+++ st_telnet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# --------------------------------
+# VMODEM - Virtual Modem bootstrap
+# --------------------------------
+# Oliver Molini 2020
+#
+# Billy Stoughton II for bug fixes and contributions
+#
+# Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+#start telnet
+telnet -8
