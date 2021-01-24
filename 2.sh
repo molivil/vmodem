24f0c8d914b3b6b14b16d02b32b6c114f4056da2
molivil <61817271+molivil@users.noreply.github.com>
2021-01-24 17:44:56 -0600

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 #
-sendtty "Terminal type set to $TERM. Running Lynx ..."
-lynx
+ 
+echo "Hello World Demo Box!"
+echo "---------------------"
+echo
+echo "You have just successfully dialed this virtual box!"
+echo
+echo "Please enter your name: "
+read -e -p "User? " username
+echo
+echo "Hello, $username!"
+echo
+echo "Thank you for visiting! Bye!"
+sleep 2
